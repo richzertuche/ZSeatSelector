033a7b78b0ea11cd76226f8c9557032eca537a1a
Ricardo Zertuche <zertuche@digitalcoaster.mx>
2017-02-12 22:24:43 -0600

    Swift 3

--- ZSeatSelector_Swift/ZSeatSelector_Swift/ZSeatSelector.swift
+++ ZSeatSelector_Swift/ZSeatSelector_Swift/ZSeatSelector.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 protocol ZSeatSelectorDelegate {
-    func seatSelected(seat: ZSeat)
-    func getSelectedSeats(seats: NSMutableArray)
+    func seatSelected(_ seat: ZSeat)
+    func getSelectedSeats(_ seats: NSMutableArray)
 }
 
 class ZSeatSelector: UIScrollView, UIScrollViewDelegate {
@@ -26,58 +26,63 @@ class ZSeatSelector: UIScrollView, UIScrollViewDelegate {
     var selected_image      = UIImage()
     let zoomable_view       = UIView()
     var selected_seat_limit:Int = 0
+    var layout_type                = ""
     
     // MARK: - Init and Configuration
     
-    func setSeatSize(size: CGSize){
+    func setSeatSize(_ size: CGSize){
         seat_width  = size.width
         seat_height = size.height
     }
-    func setMap(map: String) {
-        
-        var initial_seat_x: Int = 0
-        var initial_seat_y: Int = 0
-        var final_width: Int = 0
-        
-        for i in 0..<map.characters.count {
-            let seat_at_position = map[i]
+    func setMap(_ map: String) {
+        if (layout_type == "Normal"){
+            var initial_seat_x: Int = 0
+            var initial_seat_y: Int = 0
+            var final_width: Int = 0
             
-            if seat_at_position == "A" {
-                createSeatButtonWithPosition(initial_seat_x, and: initial_seat_y, isAvailable: true, isDisabled: false)
-                initial_seat_x += 1
-            } else if seat_at_position == "D" {
-                createSeatButtonWithPosition(initial_seat_x, and: initial_seat_y, isAvailable: true, isDisabled: true)
-                initial_seat_x += 1
-            } else if seat_at_position == "U" {
-                createSeatButtonWithPosition(initial_seat_x, and: initial_seat_y, isAvailable: false, isDisabled: false)
-                initial_seat_x += 1
-            } else if seat_at_position == "_" {
-                initial_seat_x += 1
-            } else {
-                if initial_seat_x > final_width {
-                    final_width = initial_seat_x
+            for i in 0..<map.characters.count {
+                let seat_at_position = map[i]
+                
+                if seat_at_position == "A" {
+                    createSeatButtonWithPosition(initial_seat_x, and: initial_seat_y, isAvailable: true, isDisabled: false)
+                    initial_seat_x += 1
+                } else if seat_at_position == "D" {
+                    createSeatButtonWithPosition(initial_seat_x, and: initial_seat_y, isAvailable: true, isDisabled: true)
+                    initial_seat_x += 1
+                } else if seat_at_position == "U" {
+                    createSeatButtonWithPosition(initial_seat_x, and: initial_seat_y, isAvailable: false, isDisabled: false)
+                    initial_seat_x += 1
+                } else if seat_at_position == "_" {
+                    initial_seat_x += 1
+                } else {
+                    if initial_seat_x > final_width {
+                        final_width = initial_seat_x
+                    }
+                    initial_seat_x = 0
+                    initial_seat_y += 1
                 }
-                initial_seat_x = 0
-                initial_seat_y += 1
             }
+            
+            zoomable_view.frame = CGRect(x: 0, y: 0, width: CGFloat(final_width) * seat_width, height: CGFloat(initial_seat_y) * seat_height)
+            self.contentSize = zoomable_view.frame.size
+            let newContentOffsetX: CGFloat = (self.contentSize.width - self.frame.size.width) / 2
+            self.contentOffset = CGPoint(x: newContentOffsetX, y: 0)
+            selected_seats = NSMutableArray()
+            
+            self.delegate = self
+            self.addSubview(zoomable_view)
+        } else {
+           
         }
         
-        zoomable_view.frame = CGRect(x: 0, y: 0, width: CGFloat(final_width) * seat_width, height: CGFloat(initial_seat_y) * seat_height)
-        self.contentSize = zoomable_view.frame.size
-        let newContentOffsetX: CGFloat = (self.contentSize.width - self.frame.size.width) / 2
-        self.contentOffset = CGPointMake(newContentOffsetX, 0)
-        selected_seats = NSMutableArray()
-        
-        self.delegate = self
-        self.addSubview(zoomable_view)
     }
-    func createSeatButtonWithPosition(initial_seat_x: Int, and initial_seat_y: Int, isAvailable available: Bool, isDisabled disabled: Bool) {
+    func createSeatButtonWithPosition(_ initial_seat_x: Int, and initial_seat_y: Int, isAvailable available: Bool, isDisabled disabled: Bool) {
     
-        let seatButton = ZSeat(frame: CGRectMake(
-                CGFloat(initial_seat_x) * seat_width,
-                CGFloat(initial_seat_y) * seat_height,
-                CGFloat(seat_width),
-                CGFloat(seat_height)))
+        let seatButton = ZSeat(frame: CGRect(
+                x: CGFloat(initial_seat_x) * seat_width,
+                y: CGFloat(initial_seat_y) * seat_height,
+                width: CGFloat(seat_width),
+                height: CGFloat(seat_height)))
         if available && disabled {
             self.setSeatAsDisabled(seatButton)
         }
@@ -94,24 +99,24 @@ class ZSeatSelector: UIScrollView, UIScrollViewDelegate {
         seatButton.row = initial_seat_y + 1
         seatButton.column = initial_seat_x + 1
         seatButton.price = seat_price
-        seatButton.addTarget(self, action: "seatSelected:", forControlEvents: .TouchDown)
+        seatButton.addTarget(self, action: #selector(ZSeatSelector.seatSelected(_:)), for: .touchDown)
         zoomable_view.addSubview(seatButton)
     }
     
     // MARK: - Seat Selector Methods
     
-    func seatSelected(sender: ZSeat) {
+    func seatSelected(_ sender: ZSeat) {
         if !sender.selected_seat && sender.available {
             if selected_seat_limit != 0 {
                 checkSeatLimitWithSeat(sender)
             }
             else {
                 self.setSeatAsSelected(sender)
-                selected_seats.addObject(sender)
+                selected_seats.add(sender)
             }
         }
         else {
-            selected_seats.removeObject(sender)
+            selected_seats.remove(sender)
             if sender.available && sender.disabled {
                 self.setSeatAsDisabled(sender)
             }
@@ -125,10 +130,10 @@ class ZSeatSelector: UIScrollView, UIScrollViewDelegate {
         seatSelectorDelegate?.seatSelected(sender)
         seatSelectorDelegate?.getSelectedSeats(selected_seats)
     }
-    func checkSeatLimitWithSeat(sender: ZSeat) {
+    func checkSeatLimitWithSeat(_ sender: ZSeat) {
         if selected_seats.count < selected_seat_limit {
             setSeatAsSelected(sender)
-            selected_seats.addObject(sender)
+            selected_seats.add(sender)
         }
         else {
             let seat_to_make_avaiable: ZSeat = selected_seats[0] as! ZSeat
@@ -138,47 +143,47 @@ class ZSeatSelector: UIScrollView, UIScrollViewDelegate {
             else {
                 self.setSeatAsAvaiable(seat_to_make_avaiable)
             }
-            selected_seats.removeObjectAtIndex(0)
+            selected_seats.removeObject(at: 0)
             self.setSeatAsSelected(sender)
-            selected_seats.addObject(sender)
+            selected_seats.add(sender)
         }
     }
     
     // MARK: - Seat Images & Availability
     
-    func setAvailableImage(available_image: UIImage, andUnavailableImage unavailable_image: UIImage, andDisabledImage disabled_image: UIImage, andSelectedImage selected_image: UIImage) {
+    func setAvailableImage(_ available_image: UIImage, andUnavailableImage unavailable_image: UIImage, andDisabledImage disabled_image: UIImage, andSelectedImage selected_image: UIImage) {
         self.available_image = available_image
         self.unavailable_image = unavailable_image
         self.disabled_image = disabled_image
         self.selected_image = selected_image
     }
-    func setSeatAsUnavaiable(sender: ZSeat) {
-        sender.setImage(unavailable_image, forState: .Normal)
+    func setSeatAsUnavaiable(_ sender: ZSeat) {
+        sender.setImage(unavailable_image, for: UIControlState())
         sender.selected_seat = true
     }
-    func setSeatAsAvaiable(sender: ZSeat) {
-        sender.setImage(available_image, forState: .Normal)
+    func setSeatAsAvaiable(_ sender: ZSeat) {
+        sender.setImage(available_image, for: UIControlState())
         sender.selected_seat = false
     }
-    func setSeatAsDisabled(sender: ZSeat) {
-        sender.setImage(disabled_image, forState: .Normal)
+    func setSeatAsDisabled(_ sender: ZSeat) {
+        sender.setImage(disabled_image, for: UIControlState())
         sender.selected_seat = false
     }
-    func setSeatAsSelected(sender: ZSeat) {
-        sender.setImage(selected_image, forState: .Normal)
+    func setSeatAsSelected(_ sender: ZSeat) {
+        sender.setImage(selected_image, for: UIControlState())
         sender.selected_seat = true
     }
     
     // MARK: - UIScrollViewDelegate
     
-    func scrollViewDidZoom(scrollView: UIScrollView) {
-        //print("zoom")
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        print("zoom")
     }
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return self.subviews[0]
     }
-    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
-        //print(scale)
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        print(scale)
     }
 }
 
@@ -194,6 +199,6 @@ class ZSeat: UIButton {
 
 extension String {
     subscript (i: Int) -> Character {
-        return self[self.startIndex.advancedBy(i)]
+        return self[self.characters.index(self.startIndex, offsetBy: i)]
     }
 }
